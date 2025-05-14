76612a6ae5f61c8fb66932aa01a4fb8d2b603fe6
Shee Yang <chewsheeyang@gmail.com>
2025-05-14 22:29:48 +0800

    Make it work with https

---
+++ server.mjs
@@ -0,0 +1,27 @@
+import { createServer } from 'https';
+import { parse } from 'url';
+import next from 'next';
+import { readFileSync } from 'fs';
+
+const port = parseInt(process.env.PORT || '3000', 10);
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const options = {
+  key: readFileSync('certificates/10.0.0.1-key.pem'),
+  cert: readFileSync('certificates/10.0.0.1.pem'),
+};
+
+app.prepare().then(() => {
+  createServer(options, (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port);
+
+  console.log(
+    `> Server listening at https://localhost:${port} as ${
+      dev ? 'development' : process.env.NODE_ENV
+    }`,
+  );
+});
