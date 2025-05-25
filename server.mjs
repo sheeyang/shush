97c3abd191a4024c262a7040c68bb6997751d481
Shee Yang <chewsheeyang@gmail.com>
2025-05-25 20:20:24 +0800

    Dev is now served over http

--- server.mjs
+++ server.mjs
@@ -1,4 +1,5 @@
-import { createServer } from 'https';
+import { createServer as createHttpsServer } from 'https';
+import { createServer as createHttpServer } from 'http';
 import { parse } from 'url';
 import next from 'next';
 import { readFileSync } from 'fs';
@@ -9,19 +10,27 @@ const turbo = process.env.TURBO_PACK === 'true';
 const app = next({ dev, turbo });
 const handle = app.getRequestHandler();
 
-// TODO: make it so that certifcates are optional
-const options = {
-  key: readFileSync('certificates/key.pem'),
-  cert: readFileSync('certificates/cert.pem'),
-};
-
 app.prepare().then(() => {
-  createServer(options, (req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
-  }).listen(port);
+  if (dev) {
+    // Use HTTP for development
+    createHttpServer((req, res) => {
+      const parsedUrl = parse(req.url, true);
+      handle(req, res, parsedUrl);
+    }).listen(port);
+  } else {
+    // Use HTTPS for production
+    const options = {
+      key: readFileSync('certificates/key.pem'),
+      cert: readFileSync('certificates/cert.pem'),
+    };
+
+    createHttpsServer(options, (req, res) => {
+      const parsedUrl = parse(req.url, true);
+      handle(req, res, parsedUrl);
+    }).listen(port);
+  }
 
   console.log(
-    `> Server started with port ${port} and ${turbo ? 'Turbopack' : 'Webpack'} in ${dev ? 'development' : 'production'} mode`,
+    `> Server started on ${dev ? 'http' : 'https'}://localhost:${port} and ${turbo ? 'Turbopack' : 'Webpack'} in ${dev ? 'development' : 'production'} mode.`,
   );
 });
